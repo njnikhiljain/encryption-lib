b3e0af35b6a189a5cf10dfafffd4167ea9bb3965
pardeep singh <pardeep.singh@techrbm.com>
2025-02-05 10:13:58 +0530

    Upload the library

--- encryption/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import generate_token_view, verify_token_view
-
-urlpatterns = [
-    path('generate/', generate_token_view, name="generate_token"),
-    path('verify/', verify_token_view, name="verify_token"),
-]
